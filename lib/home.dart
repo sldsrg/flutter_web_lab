7605762b4a651984ee1fe08c795936e974acf533
Sergey Solodukhin <soloss@yandex.ru>
2019-07-31 22:43:41 +0300

    add template for next lab

--- lib/home.dart
+++ lib/home.dart
@@ -2,39 +2,33 @@ import 'package:flutter_web/material.dart';
 import 'package:web_lab/markdown_lab.dart';
 
 import 'multi_grid_lab.dart';
+import 'next_lab.dart';
 import 'table_lab.dart';
+
+final builders = {
+  'Next lab': (_) => NextLabPage(),
+  'Markdown lab': (_) => MarkdownPage(),
+  'Table lab': (_) => TableLabPage(),
+  'Multi GridView lab': (_) => MultiGridLabPage(),
+};
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(
-        children: <Widget>[
-          ListTile(
-            title: Text('Markdown lab'),
-            onTap: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(builder: (_) => MarkdownPage()),
-              );
-            },
-          ),
-          ListTile(
-            title: Text('Table lab'),
-            onTap: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(builder: (_) => TableLabPage()),
-              );
-            },
-          ),
-          ListTile(
-            title: Text('Multi GridView lab'),
-            onTap: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(builder: (_) => MultiGridLabPage()),
-              );
-            },
-          )
-        ],
+        children: builders.entries
+            .map(
+              (entry) => ListTile(
+                    title: Text(entry.key),
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(builder: entry.value),
+                      );
+                    },
+                  ),
+            )
+            .toList(),
       ),
     );
   }
---
+++ lib/next_lab.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_web/material.dart';
+
+class NextLabPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
