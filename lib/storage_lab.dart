80fe982d25525cc632f1b400daabc92372f91c13
Sergey Solodukhin <soloss@yandex.ru>
2019-09-22 08:14:19 +0300

    play with image picker

--- lib/storage_lab.dart
+++ lib/storage_lab.dart
@@ -10,6 +10,32 @@ class _StorageLabPageState extends State<StorageLabPage> {
   final myController = TextEditingController();
   html.Storage _localStorage;
 
+  String file = '';
+  String error = '';
+  String data = '';
+  pick() {
+    final html.InputElement input = html.document.createElement('input');
+    input
+      ..type = 'file'
+      ..accept = 'image/*';
+
+    input.onChange.listen((e) {
+      var reader = html.FileReader();
+      reader.readAsDataUrl(input.files[0]);
+      reader.onError.listen((error) => setState(() {
+            error = error;
+          }));
+      reader.onLoad.first.then((res) {
+        setState(() {
+          file = input.files[0].name;
+          data = reader.result as String;
+        });
+      });
+    });
+
+    input.click();
+  }
+
   String test;
 
   @override
@@ -32,11 +58,24 @@ class _StorageLabPageState extends State<StorageLabPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text(file),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.play_arrow),
+        onPressed: () {
+          pick();
+        },
+      ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: TextField(
-          controller: myController,
+        child: Column(
+          children: <Widget>[
+            Text(file),
+            TextField(
+              controller: myController,
+            ),
+          ],
         ),
       ),
     );
