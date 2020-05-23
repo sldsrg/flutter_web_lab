544f4fa8409a6819d4eb9e945059005a8351a710
Sergey Solodukhin <soloss@yandex.ru>
2020-05-23 09:25:26 +0300

    update Bpmn lab

--- lib/bpmn_lab.dart
+++ lib/bpmn_lab.dart
@@ -1,4 +1,5 @@
 import 'dart:ui' as ui;
+import 'dart:js' as js;
 import 'package:universal_html/html.dart' as html;
 import 'package:flutter/material.dart';
 
@@ -9,32 +10,34 @@ class BpmnLabPage extends StatefulWidget {
 
 class _BpmnLabPageState extends State<BpmnLabPage> {
   html.DivElement _element;
+  js.JsObject _viewer;
 
   @override
   void initState() {
     super.initState();
-
-    _element = html.DivElement()
-      ..id = 'canvas'
-      ..append(html.ScriptElement()
-        ..text = """
-        const canvas = document.querySelector("flt-platform-view").shadowRoot.querySelector("#canvas");
-        const viewer = new BpmnJS({ container: canvas });
-        const uri = "https://cdn.staticaly.com/gh/bpmn-io/bpmn-js-examples/dfceecba/url-viewer/resources/pizza-collaboration.bpmn";
-        fetch(uri).then(res => res.text().then(xml => viewer.importXML(xml)));
-        """);
-
+    _element = html.DivElement();
+    _viewer = js.JsObject(
+      js.context['BpmnJS'],
+      [
+        js.JsObject.jsify({'container': _element})
+      ],
+    );
     // ignore: undefined_prefixed_name
-    ui.platformViewRegistry
-        .registerViewFactory('bpmn-view', (int viewId) => _element);
+    ui.platformViewRegistry.registerViewFactory('bpmn-view', (int viewId) => _element);
+    loadDiagram('assets/pizza-collaboration.bpmn');
+  }
+
+  loadDiagram(String src) async {
+    final bundle = DefaultAssetBundle.of(context);
+    final xml = await bundle.loadString(src);
+    _viewer.callMethod('importXML', [xml]);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Center(
-          child: HtmlElementView(key: UniqueKey(), viewType: "bpmn-view")),
+      body: Center(child: HtmlElementView(key: UniqueKey(), viewType: "bpmn-view")),
     );
   }
 }
